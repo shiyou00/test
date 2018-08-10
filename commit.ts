3bd2d78a955204a728b7f4256e3b4e0381fe3c4f
shiyou <8533334@qq.com>
2018-08-10 14:31:28 +0800

    add a new file

---
+++ commit.ts
@@ -0,0 +1,11 @@
+class Animal{
+    name : string;
+    constructor(name){
+        this.name = name;
+    }
+    sayHi(){
+        return `My name is ${this.name}`
+    }
+}
+
+let a = new Animal('jack');
